57a40d922e5002c88eb20787ec3755b5f5d311e5
Evandro <evandrocalado07@gmail.com>
2023-01-26 16:20:12 -0300

    dotenv config

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import connectDatabase from "./src/database/db.js";
 import userRoute from "./src/routes/user.route.js";
+import dotenv from "dotenv"
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
+dotenv.config()
 
 mongoose.set("strictQuery", false);
 connectDatabase();
--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,7 @@ const connectDatabase = () => {
 
   mongoose
     .connect(
-      "mongodb+srv://evandro:24122006@cluster0.gwodtve.mongodb.net/?retryWrites=true&w=majority",
+      process.env.MONGODB_URI,
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log("MongoDB Atlas Connected"))
